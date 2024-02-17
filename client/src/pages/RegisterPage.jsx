d9239e69b349226d1c9fd93c049d40f9c9d1f760
Naman Thanki <stuniz7855@gmail.com>
2024-02-17 11:40:21 +0530

    included env file

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,57 @@
-const RegisterPage = () => {
-	return <div>Register</div>;
-};
+import axios from "axios";
+import { useState } from "react";
+
+function RegisterPage() {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const handleChange = (e) => {
+		e.preventDefault();
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		axios
+			.post("http://localhost:3000/api/v1/auth/register", formData)
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	return (
+		<>
+			<div className="container">
+				<h1>Register</h1>
+				<div>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="email">Email</label>
+						<input
+							className="form-control"
+							onChange={handleChange}
+							type="text"
+							name="email"
+						/>
+						<label htmlFor="password">Password</label>
+						<input
+							className="form-control"
+							onChange={handleChange}
+							type="password"
+							name="password"
+						/>
+						<button className="btn btn-primary" type="submit">
+							Register
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
+	);
+}
 
 export default RegisterPage;
